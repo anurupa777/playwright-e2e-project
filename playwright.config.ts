bff6a1cee5afb896e89f7085093dad89923e580b
anurupa777 <das.anurupa777@gmail.com>
2026-08-16 18:40:47 +0530

    changed playwright.config.ts- removed meta import

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,6 @@ import { defineConfig, devices } from "@playwright/test";
  */
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
